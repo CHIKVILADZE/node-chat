124d6a800e82e6371dcbfdf998496746cd4dce43
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-06-19 18:33:20 +0400

    deploy backend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post('/authenticate', async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username },
+      { headers: { 'PRIVATE-KEY': '23eed870-dafe-425a-973c-2bae487f13a1' } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
