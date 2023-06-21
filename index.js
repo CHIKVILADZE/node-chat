89c02cd922ee82f70a46414a7b4a67cf1f634163
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-06-21 16:48:29 +0400

    change .env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-import axios from 'axios';
+const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
@@ -21,6 +21,6 @@ app.post('/authenticate', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(3001, () => {
+  console.log('Server is running on port 300');
 });
